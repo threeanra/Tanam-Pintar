9ee65d54307e452e96bb504a29f8f191b201f956
TEARSDR0P <trian.radis448@gmail.com>
2024-05-26 21:42:10 +0700

    the beginning

---
+++ app/src/main/java/com/capstone/tanampintar/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.capstone.tanampintar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.tanampintar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.bottomNavigation.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+        }
+
+        setContentView(binding.root)
+    }
+}
