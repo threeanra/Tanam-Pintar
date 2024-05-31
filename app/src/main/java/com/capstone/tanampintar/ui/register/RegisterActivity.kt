730719a72d22c2bbdf55798905370ee38cb834ed
TEARSDR0P <trian.radis448@gmail.com>
2024-06-01 05:27:29 +0700

    revision of the login and register  component

---
+++ app/src/main/java/com/capstone/tanampintar/ui/register/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package com.capstone.tanampintar.ui.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.tanampintar.R
+import com.capstone.tanampintar.databinding.ActivityLoginBinding
+import com.capstone.tanampintar.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
